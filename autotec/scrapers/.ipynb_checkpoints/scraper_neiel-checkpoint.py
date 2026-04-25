75a36c5dbf8839479cbf7c981748fb10563c1df0
neielcortesm <neiel.cortes@alumnos.ucn.cl>
2026-04-25 14:05:48 -0400

    Arreglo scraper_neiel.py

--- autotec/scrapers/.ipynb_checkpoints/scraper_neiel-checkpoint.py
+++ autotec/scrapers/.ipynb_checkpoints/scraper_neiel-checkpoint.py
@@ -8,8 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-NOMBRE_GRUPO = ""
-USUARIO = ""
+NOMBRE_GRUPO = "AutoTec"
+USUARIO = "Neiel C"
 
 
 def limpiar_numero(texto):
