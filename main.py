9fe16b6e369d03a647e5301d21e74b82c2f6c456
neielcortesm <neiel.cortes@alumnos.ucn.cl>
2026-04-27 19:47:08 -0400

    Ajusta main.py

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 from pyspark.sql import SparkSession
-from autotec.scrapers import scraper_dani, scraper_neiel
 from pyspark.sql.functions import col, regexp_replace
+from functools import reduce
+from autotec.scrapers import scraper_dani, scraper_neiel, scraper_martin, scraper_belenandrades
 
-# 1. prueba scrappers neiel dani
 data_dani = scraper_dani.ejecutar_extraccion()
 data_neiel = scraper_neiel.ejecutar_extraccion()
+#data_martin = scraper_martin.ejecutar_extraccion()
+#data_belen = scraper_belenandrades.ejecutar_extraccion()
 
-# 2. Iniciamos Spark
 spark = (
     SparkSession.builder
     .appName("IntegradoraBigData")
-    .config("spark.mongodb.output.uri", "mongodb+srv://neiel_cortes:neiel0330@cluster0.eo0kyfv.mongodb.net/AutoTec_db?retryWrites=true&w=majority")
+    .config("spark.mongodb.write.connection.uri", "mongodb+srv://neiel_cortes:neiel0330@cluster0.eo0kyfv.mongodb.net/AutoTec_db?retryWrites=true&w=majority")
     .getOrCreate()
 )
 
-# 3. Spark convierte las listas en un solo DataFrame unificado
 df_dani = spark.createDataFrame(data_dani)
 df_neiel = spark.createDataFrame(data_neiel)
+#df_martin = spark.createDataFrame(data_martin)
+#df_belen = spark.createDataFrame(data_belen)
 
-df_final = df_neiel.union(df_dani)
-print(f"DataFrame final: {df_final.count()} registros")
-# 4. Acción de Spark: limpieza y transformación
+df_final = reduce(lambda a, b: a.unionByName(b, allowMissingColumns=True),
+                  [df_dani, df_neiel])
+
 df_limpio = df_final.withColumn(
     "valor_numerico",
-    regexp_replace(col("valor"), "[^0-9]", "").cast("float")
+    regexp_replace(col("valor"), "[^0-9]", "").cast("double")
 )
 
-# 5. Spark guarda todo de un solo golpe en MongoDB
-print("Guardando en MongoDB...")
 df_limpio.write \
     .format("mongodb") \
     .mode("append") \
-    .option("database", "autotec_db") \
-    .option("collection", "productos_unificados") \
+    .option("spark.mongodb.write.connection.uri", "mongodb+srv://neiel_cortes:neiel0330@cluster0.eo0kyfv.mongodb.net/AutoTec_db?retryWrites=true&w=majority") \
+    .option("spark.mongodb.write.database", "proyecto_bigdata") \
+    .option("spark.mongodb.write.collection", "lista_autos") \
     .save()
 
 print("¡Proceso completado!")
