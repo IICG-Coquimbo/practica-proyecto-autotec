7fad4817af9fcabe7a6da99e1a2ca7294a110ce8
neielcortesm <neiel.cortes@alumnos.ucn.cl>
2026-04-27 21:20:03 -0400

    merge de scraper neiel, daniela, belen, martin

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from autotec.scrapers import scraper_dani, scraper_neiel, scraper_martin, scrape
 
 data_dani = scraper_dani.ejecutar_extraccion()
 data_neiel = scraper_neiel.ejecutar_extraccion()
-#data_martin = scraper_martin.ejecutar_extraccion()
-#data_belen = scraper_belenandrades.ejecutar_extraccion()
+data_martin = scraper_martin.ejecutar_extraccion()
+data_belen = scraper_belenandrades.ejecutar_extraccion()
 
 spark = (
     SparkSession.builder
@@ -17,18 +17,18 @@ spark = (
 
 df_dani = spark.createDataFrame(data_dani)
 df_neiel = spark.createDataFrame(data_neiel)
-#df_martin = spark.createDataFrame(data_martin)
-#df_belen = spark.createDataFrame(data_belen)
+df_martin = spark.createDataFrame(data_martin)
+df_belen = spark.createDataFrame(data_belen)
 
 df_final = reduce(lambda a, b: a.unionByName(b, allowMissingColumns=True),
-                  [df_dani, df_neiel])
+                  [df_dani, df_neiel, df_martin, df_belen])
 
-df_limpio = df_final.withColumn(
-    "valor_numerico",
-    regexp_replace(col("valor"), "[^0-9]", "").cast("double")
-)
+#df_limpio = df_final.withColumn(
+ #   "valor_numerico",
+  #  regexp_replace(col("valor"), "[^0-9]", "").cast("double")
+#)
 
-df_limpio.write \
+df_final.write \
     .format("mongodb") \
     .mode("append") \
     .option("spark.mongodb.write.connection.uri", "mongodb+srv://neiel_cortes:neiel0330@cluster0.eo0kyfv.mongodb.net/AutoTec_db?retryWrites=true&w=majority") \
